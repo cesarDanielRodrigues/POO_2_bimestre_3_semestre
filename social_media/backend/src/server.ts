58c423bc81bf7d8b2c161568f3d308d4855abecf
César Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2023-04-20 20:39:43 -0300

    commit primeiro aula poo

--- social_media/backend/src/server.ts
+++ social_media/backend/src/server.ts
@@ -1,1 +1,29 @@
-console.log('Projeto configurado e ok')
+
+// importa classe fastfy da dependecia fastfy
+import Fastify from "fastify";
+
+//instaciar um objeto da classe fastfy
+const app = Fastify()
+
+// criar uma rota de api com o verbo get - consulta
+app.get('/hello', ()=> {
+    return 'Hello world, good night'
+})
+
+//Rota nova
+app.get('/test',()=>{
+    return 'Teste de rota'
+})
+
+//subir o servidor http
+app.listen({
+    port:3333
+})
+.then(()=>{
+    console.log('Http server running')
+})
+
+
+
+
+
