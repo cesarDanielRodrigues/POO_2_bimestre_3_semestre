6ffd50a069f62f8464814f540d32a3c8d89c7523
César Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2023-04-28 20:24:02 -0300

    update aula

--- social_media/backend/src/server.ts
+++ social_media/backend/src/server.ts
@@ -1,29 +1,55 @@
 
 // importa classe fastfy da dependecia fastfy
 import Fastify from "fastify";
-
 //instaciar um objeto da classe fastfy
-const app = Fastify()
+const server = Fastify()
+
+//importa dependencia prisma
+import { PrismaClient } from "@prisma/client";
+// cria um objeto da classe prisma
+const prisma = new PrismaClient()
 
 // criar uma rota de api com o verbo get - consulta
-app.get('/hello', ()=> {
+server.get('/hello', () => {
     return 'Hello world, good night'
 })
 
+
 //Rota nova
-app.get('/test',()=>{
+server.get('/test', () => {
     return 'Teste de rota'
 })
 
-//subir o servidor http
-app.listen({
-    port:3333
+
+// rota para listar (consultar) os posts cadastrados no banco de dados
+// a função é assiscrona, isto é, quem a chamar, pode continuar sem que tenha  resposta
+server.get('/post', async () => {
+    //await indica que a função somente continua depois que os dados vierem do BD 
+    const posts = await prisma.post.findMany()
+    return posts
 })
-.then(()=>{
-    console.log('Http server running')
+
+
+server.get('/post/title', async () => {
+    const posts = await prisma.post.findMany({
+        where: {
+            title: {
+                startsWith: "teste"
+            }
+        }
+    })
+    return posts
 })
+
+//subir o servidor http e fica ouvindo na porta 3333
+server.listen({
+    port: 3333
+})
+    .then(() => {
+        console.log('Http server running')
+    })
 
 // ORM(prisma) - Object-Relational Mapping(Mapeamento Objeto-Relacional) é uma técnica para aproximar o paradigma de desenvolvimento de aplicações orientadas a objetos ao paradigma do banco de dados relacional.
 
 
-
+// Crud - Create(post),retriv
