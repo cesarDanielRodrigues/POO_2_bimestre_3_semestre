e8f80299530fcb82f8d6b49559149b963291a301
César Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2023-05-03 20:22:14 -0300

    update aula

--- social_media/backend/src/server.ts
+++ social_media/backend/src/server.ts
@@ -3,11 +3,15 @@
 import Fastify from "fastify";
 //instaciar um objeto da classe fastfy
 const server = Fastify()
-
+//importa a dependencia zod
+import { z } from "zod";
 //importa dependencia prisma
 import { PrismaClient } from "@prisma/client";
+
+
 // cria um objeto da classe prisma
 const prisma = new PrismaClient()
+
 
 // criar uma rota de api com o verbo get - consulta
 server.get('/hello', () => {
@@ -30,15 +34,27 @@ server.get('/post', async () => {
 })
 
 
-server.get('/post/title', async () => {
+server.get('/post/title/:title', async (request) => {
+    //define um objeto zod contando o esquema de dado
+    const titleParam = z.object({
+        title: z.string()
+    })
+    //recuperar o dado do frontend a partir do zod titleParam
+    //request.params é para recuperar o dado da url
+    //converte o texto enviando pelo frontend para a variavel teste
+    const {title} = titleParam.parse(request.params)
     const posts = await prisma.post.findMany({
         where: {
             title: {
-                startsWith: "teste"
+                startsWith: title
             }
         }
     })
     return posts
+})
+//rota para a criação de um post - verbo post
+server.post('/post', async(Request) =>{
+
 })
 
 //subir o servidor http e fica ouvindo na porta 3333
@@ -50,6 +66,5 @@ server.listen({
     })
 
 // ORM(prisma) - Object-Relational Mapping(Mapeamento Objeto-Relacional) é uma técnica para aproximar o paradigma de desenvolvimento de aplicações orientadas a objetos ao paradigma do banco de dados relacional.
-
-
 // Crud - Create(post),retriv
+//npm install zod é usado para pegar as informações do frontend
