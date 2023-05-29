d60080390044d465cf68c1b9f57b6bc6f0c6d2ba
César Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2023-05-29 18:21:48 -0300

    update aula

--- social_media/frontend/script.js
+++ social_media/frontend/script.js
@@ -14,3 +14,30 @@ async function consultaPosts() {
     //vamos jogar os dados no html
     document.getElementById('corpoTabela').innerHTML = conteudoTabela
 }
+
+//consome a api que cadastra um post no banco de dados
+async function confirmar() {
+    //recuperar os dados do formulario
+    const title = document.getElementById('title').value
+    const content = document.getElementById('content').value
+    const published = document.getElementById('sim').value
+    // alert(published)
+    const corpo = {title,content,published}
+    //chama a api
+    const post = await fetch('http://localhost:3333/post', {
+        method: 'post',
+        body: JSON.stringify(corpo), // JSON transformando em string
+        headers: {
+            "Content-Type": "application/json;charset=UTF-8"
+        }
+    })
+        .then(resposta => {
+            alert("Operação realizada com sucesso")
+        })
+        .catch(error => {
+            alert("Operação Falhou")
+        })
+        //atuliza a tabela
+        consultaPosts()
+}
+
